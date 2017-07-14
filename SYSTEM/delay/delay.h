079330d656aa2c82dabd05ae8d2a06574c43e5c2
Debang Gao <813402101@qq.com>
2017-07-14 15:26:35 +0800

    FreeRTOS stm32F7 demo
    Signed-off-by: Debang Gao <813402101@qq.com>

---
+++ SYSTEM/delay/delay.h
@@ -0,0 +1,12 @@
+#ifndef _DELAY_H
+#define _DELAY_H
+#include <sys.h>	  
+
+void delay_init(u8 SYSCLK);
+void delay_us(u32 nus);
+void delay_ms(u32 nms);
+void delay_xms(u32 nms);
+uint64_t GetSystemTimeUs(void);
+uint32_t GetSystemTimeMs(void);
+#endif
+
